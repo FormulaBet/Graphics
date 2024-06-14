a52933ca479be314430f1cd50f5ca36f30140a9a
leodefarias <lf25079@gmail.com>
2024-06-14 19:18:13 -0300

    lista pilotos

--- Gerador_de_graficos/project.py
+++ Gerador_de_graficos/project.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 
 dados_novos = pd.read_csv(r'Drivers Championship_updated.csv')
-print(dados_novos)
+
+pilotos = []
+for p in range(0, 27):
+    pilotos.append(dados_novos.loc[p, 'Participant'])
+print(pilotos)
