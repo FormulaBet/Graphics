6886d81012a185e3931700d192a06a9dd501a40d
leodefarias <lf25079@gmail.com>
2024-06-14 19:23:12 -0300

    graficos corredores

--- Gerador_de_graficos/project.py
+++ Gerador_de_graficos/project.py
@@ -13,3 +13,16 @@ equipes = []
 for t in range(0, 27):
     equipes.append(dados_novos.loc[t, 'TEAM'])
 print(equipes)
+
+for k in range(0, 27):
+    pontos = []
+    i = 9
+    while i <= 169:    
+        pontos.append(dados_novos.loc[k, dados_novos.columns[i]] + dados_novos.loc[k, dados_novos.columns[i + 5]])
+        i += 13
+
+    fig, ax = plt.subplots()
+
+    ax.plot(range(len(pontos)), pontos, marker='o')
+
+    plt.show()
